44dbb9504c5cfe088235e6dd072fcb18e616db1b
amir-ammar <am7099533@gmail.com>
2022-03-27 09:55:33 +0200

    add: files

--- gameApp/app.js
+++ gameApp/app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+
 var indexRouter = require('./routes/index');
 
 
@@ -20,7 +21,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- gameApp/routes/index.js
+++ gameApp/routes/index.js
@@ -6,6 +6,39 @@ router.get('/', function(req, res, next) {
   res.render('index');
 });
 
+router.get('/color', function(req, res){
+  res.render('game1', {results: 'Choose Color'});
+})
 
+router.get('/scissor', function(req, res){
+  res.render('game2');
+})
+
+router.get('/guess1', function(req, res){
+  var random = Math.floor(Math.random() * 3) + 1;
+  if(random == 1){
+    res.render('game1', {results: 'You Win'});
+  }else{
+    res.render('game1', {results: 'You lose'});
+  }
+});
+
+router.get('/guess2', function(req, res){
+  var random = Math.floor(Math.random() * 3) + 1;
+  if(random == 2){
+    res.render('game1', {results: 'You Win'});
+  }else{
+    res.render('game1', {results: 'You lose'});
+  }
+});
+
+router.get('/guess3', function(req, res){
+  var random = Math.floor(Math.random() * 3) + 1;
+  if(random == 3){
+    res.render('game1', {results: 'You Win'});
+  }else{
+    res.render('game1', {results: 'You lose'});
+  }
+});
 
 module.exports = router;
